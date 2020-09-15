cc4fdb27cc97739b00ecbb87b0ff04a15005f048
Richard Garsthagen <the.anykey@gmail.com>
2020-09-15 13:27:24 +0200

    Update test.py
    fix

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ region = data['canonicalRegionName']
 compID = data['compartmentId']
 instanceID = data['id']
 
-print ("region: {} ".format(region))
+print ("region: {}".format(region))
 print ("compID: {}".format(compID))
 print ("instanceID: {}".format(instanceID))
 
